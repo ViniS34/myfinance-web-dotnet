c24f2ad0c7c33c9d158535e95ee7e88a99930310
Vinicius Silva de Chaves <vinisilvachaves36@gmail.com>
2024-11-09 23:26:06 -0300

    Refactory de plano de contas.

---
+++ myfinance-web-dotnet-infra/Interfaces/IPlanoContaRepository.cs
@@ -0,0 +1,9 @@
+using myfinance_web_dotnet_domain.Entities;
+
+namespace myfinance_web_dotnet_infra.Interfaces.Base
+{
+    public interface IPlanoContaRepository : IRepository<PlanoConta>
+    {
+        
+    }
+}
